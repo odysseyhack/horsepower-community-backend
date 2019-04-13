b3ec4d199a51e20e15bb7a258936f82b44c9f363
Ingo Brenckmann <ingo.brenckmann@porschedigitallab.com>
2019-04-13 11:41:42 +0200

    communities api implemented

---
+++ horsepower-sl-community-service/communities/update.js
@@ -0,0 +1,32 @@
+import * as dynamoDbLib from "../libs/dynamodb-lib";
+import { success, failure } from "../libs/response-lib";
+
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: "communities",
+    // 'Key' defines the partition key and sort key of the item to be updated
+    // - 'userId': Identity Pool identity id of the authenticated user
+    // - 'communityId': path parameter
+    Key: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      communityId: event.pathParameters.id
+    },
+    // 'UpdateExpression' defines the attributes to be updated
+    // 'ExpressionAttributeValues' defines the value in the update expression
+    UpdateExpression: "SET content = :content",
+    ExpressionAttributeValues: {
+      ":content": data.content || null
+    },
+    // 'ReturnValues' specifies if and how to return the item's attributes,
+    // where ALL_NEW returns all attributes of the item after the update
+    ReturnValues: "ALL_NEW"
+  };
+
+  try {
+    const result = await dynamoDbLib.call("update", params);
+    return success({ status: true });
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
